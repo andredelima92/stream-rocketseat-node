457902ad22a317d69ad32db9bfa577d385b1183e
Andre <andredelima92@gmail.com>
2023-06-05 16:45:31 -0300

    application stream in http module

---
+++ fake-upload-to-http-stream.js
@@ -0,0 +1,24 @@
+import { Readable } from "node:stream";
+
+class OneToHundredStream extends Readable {
+  index = 1;
+
+  _read() {
+    const i = this.index++;
+
+    setTimeout(() => {
+      if (i > 100) {
+        this.push(null);
+      } else {
+        const buff = Buffer.from(i.toString());
+        this.push(buff);
+      }
+    }, 100);
+  }
+}
+
+fetch("http://localhost:3334", {
+  method: "POST",
+  body: new OneToHundredStream(),
+  duplex: "half",
+});
