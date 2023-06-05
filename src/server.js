330cda71a664a68787b6e122d6a4015f1e3cdebd
Andre <andredelima92@gmail.com>
2023-06-05 19:38:20 -0300

    added serevr http to lead with stream

---
+++ src/server.js
@@ -0,0 +1,40 @@
+import http from "node:http";
+
+const users = [];
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  try {
+    req.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    req.body = null;
+  }
+
+  if (method === "GET" && url === "/users") {
+    return res.setHeader("Content-type", "application/json").end(JSON.stringify(users));
+  }
+
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+    users.push({
+      id: 1,
+      name,
+      email,
+    }); //sas
+
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3333, () => {
+  console.log("Server is running...");
+});
